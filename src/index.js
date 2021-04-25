c4eb20147fd019b6efe125c5b73251ea0e8f5124
cassluvs2code <2csporvida@gmail.com>
2021-04-24 23:51:40 -0700

    setting up folder archetecture and files for being of app build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// client side rendered app: react (cra)
+  // -> database which is Firebase
+  // -> react-loading-skeleton
+  // tailwind
+
+// architecture    
+  // -> components
+  // -> constants 
+  // -> context
+  // -> helpers 
+  // -> hooks
+  // -> lib (firebase is going to live in here), 
+  // -> pages
+  // -> services (firebase functions in here)
+  // -> styles (tailwind's folder (app/tailwind))
