a0def1d1de7ea949f03eba0829bb794cfef2bf29
cassluvs2code <2csporvida@gmail.com>
2021-04-25 00:06:50 -0700

    made a few changes on my own and added a few extentions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 function App() {
   return (
-    <p>Hello</p>
+    <div className="page-wrapper">
+      <h1 className="title">CassLuvs2Code Closet <span>(an instagram clone)</span></h1>
+      <p className="description">Instagram knockoff? Hardley come in and experience this amazing social media network its like no other!</p>
+    </div>
   );
 }
 
